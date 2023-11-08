7edb72f2c05d1bd5592e307b2c9faa043a9299f3
MilaY0109 <yanovskyl@ukr.net>
2023-11-08 15:01:02 +0200

    task123

--- js/task-1.js
+++ js/task-1.js
@@ -2,6 +2,8 @@ function makeTransaction(quantity, pricePerDroid) {
     const totalPrice = quantity * pricePerDroid;
     return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
-let x = 200;
-console.log(x)
+
+console.log(makeTransaction(5, 3000)); 
+console.log(makeTransaction(3, 1000)); 
+console.log(makeTransaction(10, 500));
 
---
+++ js/task-2.js
@@ -0,0 +1,9 @@
+function getShippingMessage(country, price, deliveryFee) {
+    let totalCost = price + deliveryFee;
+    let message = `Shipping to ${country} will cost ${totalCost} credits`;
+    return message;
+}
+
+console.log(getShippingMessage("Australia", 120, 50)); 
+console.log(getShippingMessage("Germany", 80, 20)); 
+console.log(getShippingMessage("Sweden", 100, 20));
